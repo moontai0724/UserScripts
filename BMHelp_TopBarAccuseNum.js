5fadf3fb95f2c1396479b2680d486ddccd018ea6
moontai0724 <moontai0724@gmail.com>
2019-04-20 13:46:08 +0800

    更新頂端列檢舉顯示樣式至新版樣式

--- BMHelp_TopBarAccuseNum.js
+++ BMHelp_TopBarAccuseNum.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         巴哈姆特哈拉區頂端列顯示檢舉數提醒與各板檢舉數
 // @namespace    https://home.gamer.com.tw/moontai0724
-// @version      3.0
+// @version      4.0
 // @description  於巴哈姆特哈拉區頂端列顯示檢舉數提醒與各板檢舉數
 // @author       moontai0724
 // @match        https://*.gamer.com.tw/*
@@ -13,31 +13,10 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-(function () {
+(async function (jQuery) {
     'use strict';
     if (BAHAID) {
         GM_addStyle(GM_getResourceText("topBarCss"));
-
-        // 切換顯示
-        var switchDisplay = function switchDisplay(id) {
-            // 原生的按鈕如果有開啟視窗的話關閉
-            jQuery("[class*='topbnow']").removeClass('topbnow').removeClass('topbnow1').removeClass('topbnow2').removeClass('topbnow3');
-            jQuery(".TOP-msg[style*='display: block;']").each((index, element) => element.style.display = 'none');
-
-            // 關閉滑鼠不在上方的顯示
-            jQuery('#BMHelp_TopBarAccuseNum .nomouse').removeClass('nowshow');
-
-            // 切換顯示
-            document.getElementById(`BMHelp_TopBarAccuseNum_${id}`).classList.toggle('nowshow');
-        }, bodyclickevent = document.body.onclick = bodyclickevent => jQuery('#BMHelp_TopBarAccuseNum .nomouse').removeClass('nowshow');
-
-        // 當點按鈕時關閉其他視窗與效果
-        addFunction(switchDisplay);
-        addFunction(bodyclickevent);
-
-        // new
-        // 在頂端列插入新區塊
-        jQuery('<div id="BMHelp_TopBarAccuseNum"></div>').insertAfter(location.host == 'ani.gamer.com.tw' ? ".member" : ".TOP-btn:first");
 
         var list = [{
             id: 'post',
@@ -56,65 +35,98 @@
             t: 3
         }];
 
-        for (let i = 0; i < 3; i++) {
-            jQuery('#BMHelp_TopBarAccuseNum').append(`<div id="BMHelp_TopBarAccuseNum_${list[i].id}" onmouseenter="javascript:jQuery(this).removeClass('nomouse');" onmouseleave="javascript:jQuery(this).addClass('nomouse');" class="nomouse"></div>`);
-            // 新增 icon
-            let icon = document.createElement('a');
-            icon.id = `BMHelp_TopBarAccuseNum_${list[i].id}_Btn`;
-            icon.href = 'javascript:;';
-            icon.setAttribute('class', 'AccuseBtn');
-            icon.setAttribute('onclick', `javascript:switchDisplay("${list[i].id}");`);
-            document.getElementById(`BMHelp_TopBarAccuseNum_${list[i].id}`).appendChild(icon);
+        for (let i = 0; i <= 2; i++) {
+            if (location.host == "ani.gamer.com.tw") {
+                let iconsName = ["description", "list", "chat_bubble_outline"];
+                let li = document.createElement("li");
+
+                let span = document.createElement("span");
+                span.id = `topBar_accuse_${list[i].id}`;
+                span.setAttribute("class", `ani_top_accuse_${list[i].id}`);
+                li.appendChild(span);
+
+                let topIcon = document.createElement("a");
+                topIcon.href = `javascript:TOPBAR_show('accuse_${list[i].id}')`;
+                topIcon.innerHTML = `<i class="material-icons">${iconsName[i]}</i>`
+                li.appendChild(topIcon);
+
+                // 新增頂端列 icon
+                jQuery(li).insertBefore(`.member li:nth-child(${2 + i})`);
+            } else {
+                let topIcon = document.createElement("a");
+                topIcon.href = `javascript:TOPBAR_show('accuse_${list[i].id}', 'top_accuse_${list[i].id}')`;
+                topIcon.id = `topBar_accuse_${list[i].id}`;
+                topIcon.setAttribute("class", `top_accuse_${list[i].id}`);
+
+                // 新增頂端列 icon
+                jQuery(topIcon).insertBefore("#topBar_light_0");
+            }
+
 
             // 新增列表
-            let info = document.createElement('div');
-            info.id = `BMHelp_TopBarAccuseNum_${list[i].id}_Info`;
-            info.setAttribute('class', 'AccuseInfo');
-            info.innerHTML = `<span>${list[i].name}檢舉</span><div id="BMHelp_TopBarAccuseNum_${list[i].id}_List" class="AccuseList"></div><p class="TOP-msgbtn"><a href="${list[i].href}" target="_blank">看所有${list[i].name}檢舉</a></p>`;
-            document.getElementById('BMHelp_TopBarAccuseNum_' + list[i].id).appendChild(info);
+            let infoWindow = document.createElement("div");
+            infoWindow.id = `topBarMsg_accuse_${list[i].id}`;
+            infoWindow.setAttribute("class", "TOP-msg");
+            infoWindow.setAttribute("style", "display: none;");
+
+            // Title
+            let title = document.createElement("span");
+            title.innerHTML = `${list[i].name}檢舉`;
+            infoWindow.appendChild(title);
+
+            // content
+            let content = document.createElement("div");
+            content.setAttribute("class", "TOP-msglist");
+            content.id = `topBarMsgList_accuse_${list[i].id}`;
+            content.innerHTML = await getAccuseList(list[i]);
+            infoWindow.appendChild(content);
+
+            // button
+            let msgBtn = document.createElement(location.host == "ani.gamer.com.tw" ? "p" : "a");
+            msgBtn.setAttribute("class", "TOP-msgbtn");
+
+            let link = document.createElement("a");
+            link.href = list[i].href;
+            link.setAttribute("target", "_blank");
+            link.setAttribute("style", "width: auto;")
+            link.innerHTML = `<i class="fa fa-bars" aria-hidden="true"></i>看所有${list[i].name}檢舉`;
+            msgBtn.appendChild(link);
+
+            infoWindow.appendChild(msgBtn);
+
+            // 新增小窗
+            jQuery(infoWindow).insertBefore("#topBarMsg_light_0");
         }
-        setAccuseNum();
-    }
-
-    // 自訂 functions
-
-    // 創建 CSS 於網頁 head
-    function addCss(cssCode) {
-        let styleElement = document.createElement('style');
-        styleElement.type = 'text/css';
-        styleElement.styleSheet ? styleElement.styleSheet.cssText = cssCode : styleElement.appendChild(document.createTextNode(cssCode));
-        document.getElementsByTagName('head')[0].appendChild(styleElement);
-    }
-
-    // 創建 function 於網頁 body
-    // 必須要有 function 名稱及正確格式
-    function addFunction(FunctionCode) {
-        jQuery('body').append('<script type="text/javascript">' + FunctionCode + '</script>');
     }
 
     // 獲取檢舉數
-    function setAccuseNum() {
-        for (let i = 0; i < 3; i++) {
-            GetAccuseCount(list[i].t).then(data => {
-                // reset
-                document.getElementById(`BMHelp_TopBarAccuseNum_${list[i].id}_Btn`).innerHTML = '';
-                document.getElementById(`BMHelp_TopBarAccuseNum_${list[i].id}_List`).innerHTML = '';
-                jQuery(`#BMHelp_TopBarAccuseNum_${list[i].id}_Btn`).removeClass('HasAccuse');
+    function getAccuseList(type) {
+        return new Promise(function (resolve, reject) {
+            getAccuseCount(type.t).then(data => {
+                // 設定檢舉案數量提醒
+                setAccuseCount(type.id, data.total);
 
+                let accuseList = [];
                 // 當有檢舉案就加入
                 for (let board = 0; board < data.data.length; board++) {
-                    let AccuseList = document.createElement('div');
-                    AccuseList.innerHTML = `<a href="${list[i].href}&bsn=${data.data[board].bsn}" target="_blank">在 ${data.data[board].title} 哈拉板有 ${data.data[board].count} 則檢舉案。</a>`;
-                    document.getElementById(`BMHelp_TopBarAccuseNum_${list[i].id}_List`).appendChild(AccuseList);
+                    let list = document.createElement("div");
+
+                    let link = document.createElement("a");
+                    link.href = `${type.href}&bsn=${data.data[board].bsn}`;
+                    link.setAttribute("target", "_blank");
+                    link.innerHTML = `在 ${data.data[board].title} 哈拉板有 ${data.data[board].count} 則檢舉案。`;
+                    list.appendChild(link);
+
+                    accuseList.push(list.outerHTML);
                 }
 
-                // 當沒有檢舉案就顯示沒有，有檢舉案時加入數字
-                if (data.total > 0) {
-                    document.getElementById(`BMHelp_TopBarAccuseNum_${list[i].id}_Btn`).innerHTML = `<span>${data.total}</span>`;
-                    jQuery(`#BMHelp_TopBarAccuseNum_${list[i].id}_Btn`).addClass('HasAccuse');
-                } else document.getElementById(`BMHelp_TopBarAccuseNum_${list[i].id}_List`).innerHTML = `<div>太棒了！這裡沒有任何檢舉案。</div>`;
+                resolve(accuseList.length > 0 ? accuseList.join() : "<div>太棒了！這裡沒有任何檢舉案。</div>");
             });
-        }
+        });
+    }
+
+    function setAccuseCount(id, accuseCount) {
+        jQuery(`#topBar_accuse_${id}`).html(accuseCount > 0 ? (location.host == "ani.gamer.com.tw" ? accuseCount : `<span>${accuseCount}</span>`) : "");
     }
 
     // 巴哈姆特 API
@@ -122,8 +134,8 @@
     // 獲取有檢舉案之看板編號與名稱及檢舉數
     // t 為檢舉類型，1 為留言，2 為文章，3 為聊天室
     // s 為檢舉處理狀況，1 為待處理案，2 為已結案區
-    function GetAccuseCount(t) {
-        return new Promise(resolve => {
+    function getAccuseCount(t) {
+        return new Promise(function (resolve, reject) {
             GM_xmlhttpRequest({
                 method: "POST",
                 url: "https://forum.gamer.com.tw/ajax/BMaccuse_menu_2k14.php",
@@ -147,8 +159,12 @@
                         response.total += Number(data[key].num);
                     }
                     resolve(response);
+                },
+                onerror: error => {
+                    console.error(error);
+                    reject(error);
                 }
             });
         });
     }
-})();
+})(jQuery);
